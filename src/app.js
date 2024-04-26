313f5b1331e120f4e19d88a796c2b16d6bb7151e
JeffDanielle <bayotjeff@gmail.com>
2024-04-26 09:49:33 +0800

    59. Creating a First Class

---
+++ src/app.js
@@ -0,0 +1,8 @@
+var Department = /** @class */ (function () {
+    function Department(n) {
+        this.name = n;
+    }
+    return Department;
+}());
+var Accounting = new Department("Accounting");
+console.log(Accounting);
