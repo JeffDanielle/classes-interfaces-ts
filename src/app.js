e768ca243bacbd7f3b0376caf5db07d8211f92bb
JeffDanielle <bayotjeff@gmail.com>
2024-04-26 10:13:41 +0800

    612. 'private' and 'public' Access Modifiers

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     // Special parameter which refers to the property of the department class
     Department.prototype.describe = function () {
         console.log("Department: " + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department("Accounting");
+accounting.addEmployee("Jeff");
+accounting.addEmployee("Marlon");
 accounting.describe();
-var accountingCopy = { name: "DUMMY", describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = {name: "DUMMY", describe: accounting.describe};
+// accountingCopy.describe();
