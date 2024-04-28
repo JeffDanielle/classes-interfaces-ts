6ef57af867d83d8e5f87fb24800c5ec7b2ea82ec
JeffDanielle <bayotjeff@gmail.com>
2024-04-28 15:00:38 +0800

    73. Using Interfaces with Classes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
-var user1;
-user1 = {
-    name: "Jeff",
-    age: 24,
-    greet: function (phrase) {
+var Person = /** @class */ (function () {
+    function Person(n) {
+        this.name = n;
+    }
+    Person.prototype.greet = function (phrase) {
         console.log("".concat(phrase, " ").concat(this.name));
-    },
-};
+    };
+    return Person;
+}());
+var user1;
+user1 = new Person("Jeff");
 user1.greet("Hi, my name is");
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 // Interface describe structure of an object
-interface Person {
+interface Greetable {
     name: string;
-    age: number;
 
     greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+    // Must satisfy the setup of interface to be able to use it
+    name: string;
 
-user1 = {
-    name: "Jeff",
-    age: 24,
+    constructor(n: string) {
+        this.name = n;
+    }
+
     greet(phrase: string) {
         console.log(`${phrase} ${this.name}`);
-    },
-};
+    }
+}
+
+let user1: Greetable;
+
+user1 = new Person("Jeff");
 
 user1.greet("Hi, my name is");
